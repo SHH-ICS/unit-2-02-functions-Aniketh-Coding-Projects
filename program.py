ee30cb46d98ccd3c360eb53f2ea02b12e7e8d955
Aniketh-Coding-Projects <aniketh.pamu@stu.ocsb.ca>
2024-10-07 00:26:19 +0000

    all of unit 2

--- program.py
+++ program.py
@@ -1,5 +1,45 @@
-# My Python Program
-# Task:  Use the function myFunction to output a simple "Hello World!" statement
+    
 
-def myFunction():
-  # My Function
+def hello_world():
+    print("Hello World!")
+
+def hello_user(name):
+    return f"Hello, {name}!"
+
+def hello_world():
+    print("Hello World!")
+
+def hello_user(name):
+    return f"Hello, {name}!"
+
+
+hello_world()  
+print(hello_user("Aniketh")) 
+
+
+def calculate_birth_year(name, age, had_birthday):
+    current_year = 2024  
+    if had_birthday.lower() == 'yes':
+        birth_year = current_year - age
+    else:
+        birth_year = current_year - age - 1
+    return f"{name}, you were born in {birth_year}."
+
+
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+had_birthday = input("Have you had a birthday this year? (yes/no) ")
+
+
+print(calculate_birth_year(name, age, had_birthday))
+
+
+for i in range(1, 33):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
